90c496c8f88edaa32899109755d3352fe8c11435
Rachit Dani <rachitdani2014@gmail.com>
2024-07-18 22:56:10 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -8,7 +8,7 @@ class Solution:
         k = m+n -1
 
         while j>=0:
-            if i>=0 and nums1[i] > nums2[j]:
+            if nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
                 i-=1
             else:
